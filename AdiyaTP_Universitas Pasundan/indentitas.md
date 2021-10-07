ceed5daf35d8552ff7dc621d4c22415a2ce4e4ec
keirattlexnake <adiyazam03@gmail.com>
2021-10-07 22:20:28 +0700

    First submission by Adiya Tanaya Permana

---
+++ AdiyaTP_Universitas Pasundan/indentitas.md
@@ -0,0 +1,1 @@
+![Screenshot 2021-10-07 215041](https://user-images.githubusercontent.com/80138309/136409270-0841ad3d-4e6d-46f3-bada-84a7b2477663.png)
---
+++ AdiyaTP_Universitas Pasundan/program_identitas.py
@@ -0,0 +1,5 @@
+print(r"""
+nama : Adiya Tanaya Permana
+Jurusan : Informatika
+Alasan ingin masuk GDSC : untuk meningkatkan skill  dan pengetahuan di bidang informatika
+""")
